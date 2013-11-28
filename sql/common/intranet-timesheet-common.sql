797584eba767a337b941675888fefe1a137489e3
solidline <>
2013-11-28 16:36:35 +0000

    - Fixed im_absence_type view   now honoring enabled_p

--- sql/common/intranet-timesheet-common.sql
+++ sql/common/intranet-timesheet-common.sql
@@ -21,9 +21,10 @@
 -- Absences
 --
 create or replace view im_absence_types as
-select category_id as absence_type_id, category as absence_type
-from im_categories
-where category_type = 'Intranet Absence Type';
+select	category_id as absence_type_id, category as absence_type
+from	im_categories
+where	(enabled_p = 't' or enabled_p is NULL) and
+	category_type = 'Intranet Absence Type';
 
 
 
