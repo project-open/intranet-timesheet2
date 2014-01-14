1c910feb35ba6087fcca7aeaf0c9b4423c898fa5
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2014-01-14 16:57:00 +0000

    - Fixed timesheet/absence permissions

--- sql/postgresql/upgrade/upgrade-4.0.5.0.6-4.0.5.0.7.sql
+++ sql/postgresql/upgrade/upgrade-4.0.5.0.6-4.0.5.0.7.sql
@@ -1,6 +1,6 @@
--- upgrade-4.0.5.0.4-4.0.5.0.5.sql
+-- upgrade-4.0.5.0.6-4.0.5.0.7.sql
 
-SELECT acs_log__debug('/packages/intranet-timesheet2/sql/postgresql/upgrade/upgrade-4.0.5.0.4-4.0.5.0.5.sql','');
+SELECT acs_log__debug('/packages/intranet-timesheet2/sql/postgresql/upgrade/upgrade-4.0.5.0.6-4.0.5.0.7.sql','');
 
 
 -- Rename add_hours_for_direct_reports into add_hours_direct_reports
@@ -36,3 +36,20 @@ where privilege = 'add_hours_for_direct_reports';
 
 select acs_privilege__drop_privilege('add_hours_for_direct_reports');
 
+
+
+
+
+-- Delete old privilege add_hours_for_subordinates
+--
+delete from acs_permissions where privilege = 'add_hours_for_subordinates';
+select acs_privilege__drop_privilege('add_hours_for_subordinates');
+
+
+
+
+-- Add privilege to add absences for direct_reports
+select acs_privilege__create_privilege('add_absences_direct_reports','Add absences for direct reports','Add absences for direct reports');
+select acs_privilege__add_child('create', 'add_absences_direct_reports');
+select im_priv_create('add_absences_direct_reports', 'HR Managers');
+select im_priv_create('add_absences_direct_reports', 'Senior Managers');
