0e8ffb77e26570a2bf36a888dde8c204ab995c96
Malte Sussdorff <malte.sussdorff@cognovis.com>
2014-01-30 07:41:44 +0100

    Made sure the values aren't saved after we display the error
    Signed-off-by: Malte Sussdorff <malte.sussdorff@cognovis.com>

--- www/absences/new.tcl
+++ www/absences/new.tcl
@@ -509,6 +509,7 @@ ad_form -extend -name $form_id -on_request {
     # Check if the user still has the permission to edit this absence
     if {![im_absence_new_page_wf_perm_edit_button -absence_id $absence_id]} {
         ad_return_error "Not allowed to edit" "You are not allowed to edit this absence anymore. Please go <a href='$return_url'>back</a>."
+        ad_script_abort
     }
     
     # Audit the action
