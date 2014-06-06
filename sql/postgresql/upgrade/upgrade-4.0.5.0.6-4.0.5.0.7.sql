95614adebc088f4a583cb9e0d21f8f97a7256b6f
Malte Sussdorff <malte.sussdorff@cognovis.com>
2014-06-06 19:06:55 +0200

    Remaining Vacation Report
    Signed-off-by: Malte Sussdorff <malte.sussdorff@cognovis.com>

---
+++ sql/postgresql/upgrade/upgrade-4.0.5.0.6-4.0.5.0.7.sql
@@ -0,0 +1,48 @@
+-- 
+-- 
+-- 
+-- @author Malte Sussdorff (malte.sussdorff@cognovis.de)
+-- @creation-date 2013-01-14
+-- @cvs-id $Id$
+--
+
+SELECT acs_log__debug('/packages/intranet-timesheet2/sql/postgresql/upgrade/upgrade-4.0.5.0.6-4.0.5.0.7.sql','');
+
+-- ------------------------------------------------------
+--------------------------------------------------------------
+-- Remaining Vacation View
+delete from im_view_columns where view_id = 291;
+delete from im_views where view_id = 291;
+delete from im_view_columns where view_id = 1013;
+delete from im_views where view_id = 1013;
+
+
+insert into im_views (view_id, view_name, view_label) 
+values (291, 'remaining_vacation_list', 'Remaining Vacation');
+
+insert into im_view_columns (column_id, view_id, column_name, column_render_tcl, sort_order, variable_name) 
+values (29100,291,'Owner','"<a href=''/intranet-timesheet2/absences/index?user_selection=$employee_id&timescale=all&absence_type_id=$absence_type_id''>$owner_name</a>"',0,'owner_name');
+
+insert into im_view_columns (column_id, view_id, column_name, column_render_tcl, sort_order,variable_name) 
+values (29110,291,'Department Name','"$department_name"',10,'department_name');
+
+insert into im_view_columns (column_id, view_id, column_name, column_render_tcl, sort_order,variable_name) 
+values (29112,291,'Total Absence Days','$total_absence_days',12,'total_absence_days');
+
+insert into im_view_columns (column_id, view_id, column_name, column_render_tcl, sort_order,variable_name) 
+values (29123,291,'Taken Absence Days this year','$taken_absence_days_this_year',23,'taken_absence_days_this_year');
+
+insert into im_view_columns (column_id, view_id, column_name, column_render_tcl, sort_order,variable_name) 
+values (29130,291,'Remaining Absences This Year','$remaining_absence_days_this_year',30,'remaining_absence_days_this_year');
+
+insert into im_view_columns (column_id, view_id, column_name, column_render_tcl, sort_order,variable_name) 
+values (29132,291,'Requested Absence Days This year','"$requested_absence_days_this_year"',32,'requested_absence_days_this_year');
+
+insert into im_view_columns (column_id, view_id, column_name, column_render_tcl, sort_order,variable_name) 
+values (29135,291,'Entitlement Days this year','"$entitlement_days_this_year"',35,'entitlement_days_this_year');
+
+insert into im_view_columns (column_id, view_id, column_name, column_render_tcl, sort_order,variable_name) 
+values (29140,291,'Entitlement Days Total','"$entitlement_days_total"',40,'entitlement_days_total');
+
+insert into im_view_columns (column_id, view_id, column_name, column_render_tcl, sort_order,variable_name) 
+values (29144,291,'Remaining Vacation Days','"$remaining_vacation_days"',44,'remaining_vacation_days');
--- www/leave-entitlements/remaining-vacation.tcl
+++ www/leave-entitlements/remaining-vacation.tcl
@@ -18,28 +18,18 @@
 # ---------------------------------------------------------------
 
 ad_page_contract { 
-    List all projects with dimensional sliders.
 
-    @param order_by project display order 
-    @param include_subprojects_p whether to include sub projects
-    @param mine_p:
-	"t": Show only mine
-	"f": Show all projects
-	"dept": Show projects of my department(s)
+    @param absence_type_id Type of absence we want to report on
+    @param reference_date Reference Date on which we count the vacation days
+    
+    @author malte.sussdorff@cognovis.de
 
-    @param status_id criteria for project status
-    @param project_type_id criteria for project_type_id
-    @param letter criteria for im_first_letter_default_to_a(project_name)
-    @param start_idx the starting index for query
-    @param how_many how many rows to return
-
-    @author mbryzek@arsdigita.com
-    @author frank.bergmann@project-open.com
 } {
-    { order_by "Project nr" }
-    { start_date "" }
-    { view_name "project_list" }
+    { reference_date "" }
+    { view_name "remaining_vacation_list" }
+    { absence_type_id "[im_user_absence_type_vacation]"}
     { view_type "" }
+    { department_id ""}
 }
 
 
@@ -63,7 +53,7 @@ set return_url [im_url_with_query]
 # Put security code in here.
 
 
-if {"" == $start_date} { set start_date $today }
+if {"" == $reference_date} { set reference_date $today }
 
 # ---------------------------------------------------------------
 # 3. Defined Table Fields
@@ -150,8 +140,8 @@ ad_form \
     -method GET \
     -export {order_by}\
     -form {
-        {start_date:text(text) {label "[_ intranet-timesheet2.Start_Date]"} {value "$start_date"} {html {size 10}} {after_html {<input type="button" style="height:20px; width:20px; background: url('/resources/acs-templating/calendar.gif');" onclick ="return showCalendar('start_date', 'y-m-d');" >}}}
-        {absence_type_id:text(select),optional {label "[_ intranet-timesheet2.Absence_Type]"} {value $absence_type_id} {options $absence_type_list }}
+        {reference_date:text(text) {label "[_ intranet-timesheet2.Start_Date]"} {value "$reference_date"} {html {size 10}} {after_html {<input type="button" style="height:20px; width:20px; background: url('/resources/acs-templating/calendar.gif');" onclick ="return showCalendar('reference_date', 'y-m-d');" >}}}
+        {absence_type_id:text(select) {label "[_ intranet-timesheet2.Absence_Type]"} {value $absence_type_id} {options $absence_type_list }}
     }
 
 
@@ -159,27 +149,23 @@ ad_form \
 set view_type_options [list [list HTML ""]]
 
 # Run callback to extend the filter and/or add items to the view_type_options
-callback im_projects_index_filter -form_id $form_id
-ad_form -extend -name $form_id -form {
-    {view_type:text(select),optional {label "#intranet-openoffice.View_type#"} {options $view_type_options}}
-}
+#callback im_projects_index_filter -form_id $form_id
+#ad_form -extend -name $form_id -form {
+#    {view_type:text(select),optional {label "#intranet-openoffice.View_type#"} {options $view_type_options}}
+#}
 
 # ---------------------------------------------------------------
 # 5. Generate SQL Query
 # ---------------------------------------------------------------
 
 set criteria [list]
-if { ![empty_string_p $absence_type_id] && $absence_type_id > 0 } {
-    lappend criteria "absence_type_id in ([join [im_sub_categories $absence_type_id] ","])"
-}
 
+# If the user isn't HR, we can only see employees where the current user is the supervisor
 if {![im_user_is_hr_p $user_id]} {
     # Only HR can view all users, everyone only the users he is supervising
     lappend criteria "and owner_id in (select employee_id from im_employees where supervisor_id = :user_id)"
 }
 
-# Limit to start-date and end-date
-if {"" != $start_date} { lappend criteria "p.end_date::date >= :start_date" }
 
 set order_by_clause "order by lower(project_nr) DESC"
 if {$view_order_by_clause != ""} {
@@ -215,44 +201,56 @@ if { ![empty_string_p $extra_where] } {
 # Grouping should be by vacation type
 # Ordering should be by default by the owner
 
-set sql "
-    select sum(entitlement_days), sum()
 
+set booking_year [string range $reference_date 0 3]
+set eoy "${booking_year}-12-31"
+set soy "${booking_year}-01-01"
+
+# Fill Has values for each employee that is visible
+set active_category_ids [template::util::tcl_to_sql_list [im_sub_categories [im_user_absence_status_active]]]
+set requested_category_ids [template::util::tcl_to_sql_list [im_sub_categories [im_user_absence_status_requested]]]
+set sql "select employee_id,im_cost_center_name_from_id(department_id) as department_name, (
+        select coalesce(sum(duration_days),0) from im_user_absences
+        where start_date::date <= to_date(:eoy,'YYYY-MM-DD')
+        and absence_status_id in ($active_category_ids)
+        and absence_type_id = :absence_type_id
+        and employee_id = owner_id
+    ) as total_absence_days,
+    ( select coalesce(sum(duration_days),0) from im_user_absences
+        where start_date::date <= to_date(:reference_date,'YYYY-MM-DD')
+        and start_date::date >= to_date(:soy,'YYYY-MM-DD')
+        and absence_status_id in ($active_category_ids)
+        and absence_type_id = :absence_type_id
+        and employee_id = owner_id
+    ) as taken_absence_days_this_year,
+    ( select coalesce(sum(duration_days),0) from im_user_absences
+        where start_date::date > to_date(:reference_date,'YYYY-MM-DD')
+        and start_date::date <= to_date(:eoy,'YYYY-MM-DD')
+        and absence_status_id in ($active_category_ids)
+        and absence_type_id = :absence_type_id
+        and employee_id = owner_id
+    ) as remaining_absence_days_this_year,
+    ( select coalesce(sum(duration_days),0) from im_user_absences
+      where start_date::date >= to_date(:soy,'YYYY-MM-DD')
+      and start_date::date <= to_date(:eoy,'YYYY-MM-DD')
+      and absence_status_id in ($requested_category_ids)
+      and absence_type_id = :absence_type_id
+      and employee_id = owner_id
+    ) as requested_absence_days_this_year,
+    (select coalesce(sum(entitlement_days),0) from im_user_leave_entitlements 
+     where owner_id = employee_id 
+     and leave_entitlement_type_id = :absence_type_id
+     and booking_date <= to_date(:eoy,'YYYY-MM-DD')
+     and to_date(:soy,'YYYY-MM-DD') >= booking_date) as entitlement_days_this_year,
+    (select coalesce(sum(entitlement_days),0) from im_user_leave_entitlements 
+     where owner_id = employee_id
+     and leave_entitlement_type_id = :absence_type_id
+     and booking_date <= to_date(:eoy,'YYYY-MM-DD')) as entitlement_days_total
+    from im_employees e, cc_users c
+    where	c.user_id = e.employee_id
+        and c.member_state = 'approved'
+    $criteria
 "
-
-# ---------------------------------------------------------------
-# 5a. Limit the SQL query to MAX rows and provide << and >>
-# ---------------------------------------------------------------
-
-# Limit the search results to N data sets only
-# to be able to manage large sites
-#
-
-ns_log Notice "/intranet/project/index: Before limiting clause"
-
-if {[string equal $upper_letter "ALL"]} {
-    # Set these limits to negative values to deactivate them
-    set total_in_limited -1
-    set how_many -1
-    set selection $sql
-} else {
-    # We can't get around counting in advance if we want to be able to
-    # sort inside the table on the page for only those users in the
-    # query results
-    set total_in_limited [db_string total_in_limited "
-        select count(*)
-        from ($sql) s
-    "]
-
-    # Special case: FIRST the users selected the 2nd page of the results
-    # and THEN added a filter. Let's reset the results for this case:
-    while {$start_idx > 0 && $total_in_limited < $start_idx} {
-	set start_idx [expr $start_idx - $how_many]
-	set end_idx [expr $end_idx - $how_many]
-    }
-
-    set selection [im_select_row_range $sql $start_idx $end_idx]
-}	
 
 # ---------------------------------------------------------------
 # 6. Format the Filter
@@ -262,47 +260,12 @@ if {[string equal $upper_letter "ALL"]} {
 # return a table with a form in it (if there are too many
 # options
 
-ns_log Notice "/intranet/project/index: Before formatting filter"
-
-
-set mine_p_options [list \
-	[list $all_l10n "f" ] \
-	[list [lang::message::lookup "" intranet-core.With_members_of_my_dept "With member of my department"] "dept"] \
-	[list [lang::message::lookup "" intranet-core.Mine "Mine"] "t"] \
-]
-
-set letter $upper_letter
 
 # ----------------------------------------------------------
 # Do we have to show administration links?
 
 ns_log Notice "/intranet/project/index: Before admin links"
 set admin_html "<ul>"
-
-if {[im_permission $current_user_id "add_projects"]} {
-    append admin_html "<li><a href=\"/intranet/projects/new\">[_ intranet-core.Add_a_new_project]</a></li>\n"
-
-    set new_from_template_p [ad_parameter -package_id [im_package_core_id] EnableNewFromTemplateLinkP "" 0]
-    if {$new_from_template_p} {
-        append admin_html "<li><a href=\"/intranet/projects/new-from-template\">[lang::message::lookup "" intranet-core.Add_a_new_project_from_Template "Add a new project from Template"]</a></li>\n"
-    }
-
-    set wf_oid_col_exists_p [im_column_exists wf_workflows object_type]
-    if {$wf_oid_col_exists_p} {
-	set wf_sql "
-		select	t.pretty_name as wf_name,
-			w.*
-		from	wf_workflows w,
-			acs_object_types t
-		where	w.workflow_key = t.object_type
-			and w.object_type = 'im_project'
-	"
-	db_foreach wfs $wf_sql {
-	    set new_from_wf_url [export_vars -base "/intranet/projects/new" {workflow_key}]
-	    append admin_html "<li><a href=\"$new_from_wf_url\">[lang::message::lookup "" intranet-core.New_workflow "New %wf_name%"]</a></li>\n"
-	}
-    }
-}
 
 # Append user-defined menus
 set bind_vars [list return_url $return_url]
@@ -314,7 +277,6 @@ append admin_html "</ul>"
 # ---------------------------------------------------------------
 
 # Set up colspan to be the number of headers + 1 for the # column
-ns_log Notice "/intranet/project/index: Before format header"
 set colspan [expr [llength $column_headers] + 1]
 
 set table_header_html ""
@@ -322,7 +284,7 @@ set table_header_html ""
 # Format the header names with links that modify the
 # sort order of the SQL query.
 #
-set url "index?"
+set url "remaining_vacation?"
 set query_string [export_ns_set_vars url [list order_by]]
 if { ![empty_string_p $query_string] } {
     append url "$query_string&"
@@ -334,12 +296,7 @@ foreach col $column_headers {
     set wrench_html [lindex $column_headers_admin $ctr]
     regsub -all " " $col "_" col_txt
     set col_txt [lang::message::lookup "" intranet-core.$col_txt $col]
-    if {$ctr == 0 && $show_bulk_actions_p} {
-	append table_header_html "<td class=rowtitle>$col_txt$wrench_html</td>\n"
-    } else {
-	#set col [lang::util::suggest_key $col]
 	append table_header_html "<td class=rowtitle><a href=\"${url}order_by=[ns_urlencode $col]\">$col_txt</a>$wrench_html</td>\n"
-    }
     incr ctr
 }
 append table_header_html "</tr>\n"
@@ -349,155 +306,43 @@ append table_header_html "</tr>\n"
 # 8. Format the Result Data
 # ---------------------------------------------------------------
 
-ns_log Notice "/intranet/project/index: Before db_foreach"
-
 set table_body_html ""
 set bgcolor(0) " class=roweven "
 set bgcolor(1) " class=rowodd "
 set ctr 0
-set idx $start_idx
 
-callback im_projects_index_before_render -view_name $view_name \
-    -view_type $view_type -sql $selection -table_header $page_title -variable_set $form_vars
+# callback im_projects_index_before_render -view_name $view_name \
+#    -view_type $view_type -sql $selection -table_header $page_title -variable_set $form_vars
 
-db_1row timeline "
-	select	 max(end_date) as timeline_end_date,
-		 min(start_date) as timeline_start_date
-	from	 ($sql) t
-"
-
-db_foreach projects_info_query $selection -bind $form_vars {
-
-    set project_type [im_category_from_id $project_type_id]
-    set project_status [im_category_from_id $project_status_id]
-
-    # Multi-Select
-    set select_project_checkbox "<input type=checkbox name=select_project_id value=$project_id id=select_project_id,$project_id>"
-
-    set timeline_html [im_project_gantt_main_project \
-			   -timeline_start_date $timeline_start_date \
-			   -timeline_end_date $timeline_end_date \
-			   -timeline_width 400 \
-			   -project_id $project_id \
-			   -start_date $start_date \
-			   -end_date $end_date \
-			   -percent_completed $percent_completed \
-    ]
-
-    # Gif for collapsable tree?
-    set gif_html ""
-
-    set url [im_maybe_prepend_http $url]
-    if { [empty_string_p $url] } {
-        set url_string "&nbsp;"
-    } else {
-        set url_string "<a href=\"$url\">$url</a>"
-    }
+db_foreach remaining_vacation_query $sql {
+    if {$taken_absence_days_this_year == 0 && $remaining_absence_days_this_year == 0 && $requested_absence_days_this_year == 0 && $entitlement_days_this_year == 0} {continue}
     
+    set remaining_vacation_days [expr $entitlement_days_total - $total_absence_days - $requested_absence_days_this_year]
+    set owner_name [im_name_from_user_id $employee_id]
     # Append together a line of data based on the "column_vars" parameter list
     set row_html "<tr$bgcolor([expr $ctr % 2])>\n"
     foreach column_var $column_vars {
-	append row_html "\t<td valign=top>"
-	set cmd "append row_html $column_var"
-	if [catch {
-	    eval "$cmd"
-	} errmsg] {
+        append row_html "\t<td valign=top>"
+        set cmd "append row_html $column_var"
+        if [catch {
+            eval "$cmd"
+        } errmsg] {
             # TODO: warn user
-	}
-	append row_html "</td>\n"
+        }
+        append row_html "</td>\n"
     }
     append row_html "</tr>\n"
     append table_body_html $row_html
     
     incr ctr
-    if { $how_many > 0 && $ctr > $how_many } {
-        break
-    }
-    incr idx
 }
 
 # Show a reasonable message when there are no result rows:
 if { [empty_string_p $table_body_html] } {
     set table_body_html "
         <tr><td colspan=$colspan><ul><li><b> 
-	[lang::message::lookup "" intranet-core.lt_There_are_currently_n "There are currently no entries matching the selected criteria"]
-        </b></ul></td></tr>"
+No users        </b></ul></td></tr>"
 }
-
-if { $end_idx < $total_in_limited } {
-    # This means that there are rows that we decided not to return
-    # Include a link to go to the next page
-    set next_start_idx [expr $end_idx + 0]
-    set next_page_url "index?start_idx=$next_start_idx&amp;[export_ns_set_vars url [list start_idx]]"
-} else {
-    set next_page_url ""
-}
-
-if { $start_idx > 0 } {
-    # This means we didn't start with the first row - there is
-    # at least 1 previous row. add a previous page link
-    set previous_start_idx [expr $start_idx - $how_many]
-    if { $previous_start_idx < 0 } { set previous_start_idx 0 }
-    set previous_page_url "index?start_idx=$previous_start_idx&amp;[export_ns_set_vars url [list start_idx]]"
-} else {
-    set previous_page_url ""
-}
-
-# ---------------------------------------------------------------
-# 9. Format Table Continuation
-# ---------------------------------------------------------------
-
-ns_log Notice "/intranet/project/index: before table continuation"
-# Check if there are rows that we decided not to return
-# => include a link to go to the next page
-#
-if {$total_in_limited > 0 && $end_idx < $total_in_limited} {
-    set next_start_idx [expr $end_idx + 0]
-    set next_page "<a href=index?start_idx=$next_start_idx&amp;[export_ns_set_vars url [list start_idx]]>Next Page</a>"
-} else {
-    set next_page ""
-}
-
-# Check if this is the continuation of a table (we didn't start with the
-# first row - there is at least 1 previous row.
-# => add a previous page link
-#
-if { $start_idx > 0 } {
-    set previous_start_idx [expr $start_idx - $how_many]
-    if { $previous_start_idx < 0 } { set previous_start_idx 0 }
-    set previous_page "<a href=index?start_idx=$previous_start_idx&amp;[export_ns_set_vars url [list start_idx]]>Previous Page</a>"
-} else {
-    set previous_page ""
-}
-
-set table_continuation_html "
-<tr>
-  <td align=center colspan=$colspan>
-    [im_maybe_insert_link $previous_page $next_page]
-  </td>
-</tr>"
-
-if {$show_bulk_actions_p} {
-    set table_continuation_html "
-	<tr>
-	<td colspan=99>[im_project_action_select]</td>
-	</tr>
-$table_continuation_html
-    "
-}
-
-
-# ---------------------------------------------------------------
-# Dashboard column
-# ---------------------------------------------------------------
-
-set dashboard_column_html [string trim [im_component_bay "right"]]
-if {"" == $dashboard_column_html} {
-    set dashboard_column_width "0"
-} else {
-    set dashboard_column_width "250"
-}
-
 
 # ---------------------------------------------------------------
 # Navbars
@@ -518,36 +363,12 @@ foreach query_piece $query_pieces {
 }
 
 
-set start_date $org_start_date
-set end_date $org_end_date
-set project_status_id $org_project_status_id
-set project_type_id $org_project_type_id
-# !!! ad_return_complaint 1 "pass=$pass_through_vars, start_date=$start_date, end_date=$end_date"
-
-# Project Navbar goes to the top
-#
-set letter $upper_letter
-set project_navbar_html [\
-			     im_project_navbar \
-			     -current_plugin_id $plugin_id \
-			     $letter \
-			     "/intranet/projects/index" \
-			     $next_page_url \
-			     $previous_page_url \
-			     $pass_through_vars \
-			     $menu_select_label \
-			    ]
-
 # Compile and execute the formtemplate if advanced filtering is enabled.
-eval [template::adp_compile -string {<formtemplate id="project_filter" style="tiny-plain-po"></formtemplate>}]
+eval [template::adp_compile -string {<formtemplate id="$form_id" style="tiny-plain-po"></formtemplate>}]
 set filter_html $__adp_output
 
-# Customizing for Kolibri. Do not show the filter to freelancers
-if {[im_profile::member_p -user_id $user_id -profile "Freelancers"]} {
-    set left_navbar_html ""
-} else {
-    # Left Navbar is the filter/select part of the left bar
-    set left_navbar_html "
+# Left Navbar is the filter/select part of the left bar
+set left_navbar_html "
 	<div class='filter-block'>
         	<div class='filter-title'>
 	           #intranet-core.Filter_Projects# $filter_admin_html
@@ -555,13 +376,4 @@ if {[im_profile::member_p -user_id $user_id -profile "Freelancers"]} {
             	$filter_html
       	</div>
       <hr/>
-"
-}
-append left_navbar_html "
-      	<div class='filter-block'>
-        <div class='filter-title'>
-            #intranet-core.Admin_Projects#
-        </div>
-	$admin_html
-      	</div>
 "
