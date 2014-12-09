ff618059a006ac45501ae938a5fbc8b34bd4b5f1
Neophytos Demetriou <neophytos@azet.sk>
2014-12-09 14:47:51 +0100

    moved code from absences/index.tcl to im_user_timesheet_absences_options_2 (but have not replaced im_user_timesheet_absences_options)

--- www/absences/index.tcl
+++ www/absences/index.tcl
@@ -56,15 +56,12 @@ ad_page_contract {
 set user_id [ad_maybe_redirect_for_registration]
 set current_user_id $user_id
 set subsite_id [ad_conn subsite_id]
-set add_absences_for_group_p [im_permission $user_id "add_absences_for_group"]
+
 set add_absences_all_p [im_permission $user_id "add_absences_all"]
 set view_absences_all_p [expr [im_permission $user_id "view_absences_all"] || $add_absences_all_p]
 set add_absences_direct_reports_p [im_permission $user_id "add_absences_direct_reports"]
-set view_absences_direct_reports_p [expr [im_permission $user_id "view_absences_direct_reports"] || $add_absences_direct_reports_p]
-set add_absences_p [im_permission $user_id "add_absences"]
 set org_absence_type_id $absence_type_id
 set show_context_help_p 1
-set name_order [parameter::get -package_id [apm_package_id_from_key intranet-core] -parameter "NameOrder" -default 1]
 
 
 # Support if we pass a project_id in
@@ -81,51 +78,11 @@ if {!$view_absences_all_p} {
 
 set today [db_string today "select now()::date"]
 
-set all_user_options [im_user_options -include_empty_p 0 -group_name "Employees"]
-set direct_reports_options [im_user_direct_reports_options -user_id $current_user_id]
-set direct_report_ids [im_user_direct_reports_ids -user_id $current_user_id]
-
 if {"" != $user_id_from_search} { set user_selection $user_id_from_search }
 
 if {![im_permission $user_id "view_absences"] && !$view_absences_all_p && !$view_absences_direct_reports_p} { 
     ad_return_complaint 1 "You don't have permissions to see absences"
     ad_script_abort
-}
-
-
-# Setting list of "direct reports" and "other employees"
-set direct_reports_list [list]
-set other_employees_list [list]
-if { $view_absences_direct_reports_p || $add_absences_all_p || $view_absences_all_p } {
-    set emp_sql "
-	SELECT distinct
-		im_name_from_user_id(cc.user_id, $name_order) as name,
-		cc.user_id,
-		e.supervisor_id
-	FROM
-		group_member_map gm,
-		membership_rels mr,
-		acs_rels r,
-		cc_users cc,
-		im_employees e
-	WHERE
-		gm.rel_id = mr.rel_id
-		AND r.rel_id = mr.rel_id
-		AND r.rel_type = 'membership_rel'
-		AND e.employee_id = gm.member_id
-		AND cc.member_state = 'approved'
-		AND cc.user_id = gm.member_id
-		AND gm.group_id = [im_employee_group_id]
-	order by
-		name
-    "
-    db_foreach emps $emp_sql {
-        if { $supervisor_id == $current_user_id } {
-	        lappend direct_reports_list [list "&nbsp;&nbsp;$name" $user_id]
-        } else {
-	        lappend other_employees_list [list "&nbsp;&nbsp;$name" $user_id]
-        }
-    }
 }
 
 set page_title "Absences"
@@ -139,125 +96,9 @@ set user_view_page "/intranet/users/view"
 #                                                          #
 # ---------- setting filter 'User selection' ------------- # 
 
-# Users can only see their own absences, unless they have a special permission
-# ToDo: Users should _always_ see their absences 
-if {!$view_absences_all_p} { 
-    set user_selection_types [list "mine" "Mine"] 
-} else {
-    set user_selection_types [list "mine" "Mine" "all" "All"]
-}
 
+set user_selection_options [im_user_timesheet_absences_options_2 -project_id $project_id -user_selection $user_selection]
 
-set emp_sql ""
-
-# Only 'direct' subordinates. 
-if {$view_absences_direct_reports_p} { 
-    lappend user_selection_types "direct_reports"
-    lappend user_selection_types "Direct reports"
-    # Add employees to user_selection
-    set emp_sql "
-	SELECT
-        	im_name_from_user_id(cc.user_id, $name_order) as name,
-	        cc.user_id
-	FROM
-        	group_member_map gm,
-	        membership_rels mr,
-        	acs_rels r,
-	        cc_users cc, 
-                im_employees e
-	WHERE
-        	gm.rel_id = mr.rel_id
-	        AND r.rel_id = mr.rel_id
-        	AND r.rel_type = 'membership_rel'
-	        AND cc.user_id = gm.member_id
-        	AND cc.member_state = 'approved'
-	        AND cc.user_id = gm.member_id
-        	AND gm.group_id = [im_employee_group_id]
-                AND cc.user_id = e.employee_id
-                AND e.supervisor_id = :current_user_id
-	order by
-		name
-    "
-}
-
-if {$add_absences_all_p} {
-    # Add employees to user_selection
-    set emp_sql "
-	SELECT
-        	im_name_from_user_id(cc.user_id, $name_order) as name,
-	        cc.user_id
-	FROM
-        	group_member_map gm,
-	        membership_rels mr,
-        	acs_rels r,
-	        cc_users cc
-	WHERE
-        	gm.rel_id = mr.rel_id
-	        AND r.rel_id = mr.rel_id
-        	AND r.rel_type = 'membership_rel'
-	        AND cc.user_id = gm.member_id
-        	AND cc.member_state = 'approved'
-	        AND cc.user_id = gm.member_id
-        	AND gm.group_id = [im_employee_group_id]
-	order by
-		name
-    "
-
-}
-
-set cost_center_options ""
-# Deal with the departments
-if {$view_absences_all_p} {
-    set cost_center_options [im_cost_center_options -include_empty_name [lang::message::lookup "" intranet-core.All "All"] -department_only_p 0]
-} else {
-    # Limit to Cost Centers where he is the manager
-    set cost_center_options [im_cost_center_options -department_only_p 1 -manager_id $current_user_id]
-}
-
-if {"" != $cost_center_options} {
-    foreach option $cost_center_options {
-	lappend user_selection_types [lindex $option 1] 
-	lappend user_selection_types [lindex $option 0]
-    }
-}
-
-# Hide employees from the drop down box for the time being
-#db_foreach emps $emp_sql {
-#	lappend user_selection_types $user_id
-#	lappend user_selection_types $name
-#}
-
-# Deal with project managers and display their projects in this list
-
-db_foreach manager_of_project_ids "select distinct r.object_id_one, p.project_nr || ' - ' || p.project_name as project_name
-	from acs_rels r, im_biz_object_members bom, im_projects p
-	where r.object_id_two = :current_user_id
-    and r.rel_id = bom.rel_id
-    and p.project_id = r.object_id_one
-    and bom.object_role_id = [im_biz_object_role_project_manager]
-    and p.project_type_id not in (100,101)
-    union select project_id,project_name from im_projects where project_id=:project_id order by project_name" {
-    
-    lappend user_selection_types $object_id_one
-    lappend user_selection_types $project_name
-
-}
-
-# All
-if {$add_absences_all_p || $view_absences_all_p} {
-    lappend user_selection_types "employees"
-    lappend user_selection_types [lang::message::lookup "" intranet-timesheet2.Employees "Employees"] 
-    lappend user_selection_types "providers"
-    lappend user_selection_types [lang::message::lookup "" intranet-timesheet2.Providers "Providers"] 
-    lappend user_selection_types "customers"   
-    lappend user_selection_types [lang::message::lookup "" intranet-timesheet2.Customers "Customers"] 
-}
-
-# ---------------------------------------------------------------
-# Build Drop-down boxes
-# ---------------------------------------------------------------
-
-set user_selection_options [im_user_timesheet_absences_options]
 
 # ---------- / setting filter 'User selection' ------------- # 
 
@@ -288,16 +129,6 @@ foreach { value text } $absences_types {
     lappend absence_type_list [list $text $value]
 }
 
-set user_selection_in_types 0
-foreach { value text } $user_selection_types {
-    if {$value eq $user_selection} {set user_selection_in_types 1}
-    lappend user_selection_type_list [list $text $value]
-}
-
-if {$user_selection_in_types eq 0} {
-    lappend user_selection_type_list [list [im_name_from_id $user_selection] $user_selection]
-}
-
 # ---------------------------------------------------------------
 # 6. Format the Filter
 # ---------------------------------------------------------------
@@ -317,7 +148,7 @@ ad_form \
 
         {user_selection:text(select),optional
             {label "[_ intranet-timesheet2.Show_Users]"}
-            {options $user_selection_type_list}
+            {options $user_selection_options}
             {value $user_selection}}
 
         {absence_type_id:text(select),optional
@@ -360,14 +191,14 @@ if {[string is integer $user_selection]} {
 
     # Permission to log for any user - OK
     if {$add_absences_all_p} {
-	set for_user_id $user_selection
+        set for_user_id $user_selection
     }
 
     if {!$add_absences_all_p && $add_absences_direct_reports_p} {
-	set direct_reports [im_user_direct_reports_ids -user_id $current_user_id]
-	if {[lsearch $direct_reports $user_selection] > -1} {
-	    set for_user_id $user_selection
-	}
+        set direct_reports [im_user_direct_reports_ids -user_id $current_user_id]
+        if {[lsearch $direct_reports $user_selection] > -1} {
+            set for_user_id $user_selection
+        }
     }
 }
 
