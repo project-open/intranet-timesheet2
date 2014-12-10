e3de06407cae37729884bb51ade4830f56871b6d
Neophytos Demetriou <neophytos@azet.sk>
2014-12-10 14:08:49 +0100

    fixes issue introduced with upgrade script upgrade-4.0.3.0.3-4.0.3.0.4.sql, multiple global var names in global_var

--- www/absences/new.tcl
+++ www/absences/new.tcl
@@ -34,6 +34,7 @@ if {![info exists enable_master_p]} { set enable_master_p 1}
 set user_id [ad_maybe_redirect_for_registration]
 set current_user_id $user_id
 set ::absence_owner_id $user_id
+set ::current_user_id $user_id
 
 if {"" == $user_id_from_search} {set user_id_from_search $current_user_id}
 set owner_id $user_id_from_search
