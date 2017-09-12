bcef3280089047f0d62ecbaeaa09c936ddc0b6f9
Frank Bergmann <frank.bergmann@project-open.com>
2017-09-12 12:04:52 +0000

    - Fixed missing page

---
+++ www/absences/admin/index.tcl
@@ -0,0 +1,17 @@
+# /packages/intranet-core/www/admin/object-type-admin.tcl
+#
+# Copyright (C) 2004 ]project-open[
+#
+# This program is free software. You can redistribute it
+# and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation;
+# either version 2 of the License, or (at your option)
+# any later version. This program is distributed in the
+# hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+
+ad_returnredirect "/intranet/admin/"
+
+
