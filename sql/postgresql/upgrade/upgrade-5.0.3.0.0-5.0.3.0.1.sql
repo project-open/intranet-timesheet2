13f32c6aa8b96a90dd1d103bca055ca705b3977a
cosine <>
2020-01-14 13:19:22 +0000

    - Fixed l10n - Added weekend color

---
+++ sql/postgresql/upgrade/upgrade-5.0.3.0.0-5.0.3.0.1.sql
@@ -0,0 +1,12 @@
+-- upgrade-5.0.3.0.0-5.0.3.0.1.sql
+
+SELECT acs_log__debug('/packages/intranet-timesheet2/sql/postgresql/upgrade/upgrade-5.0.3.0.0-5.0.3.0.1.sql','');
+
+-- Fix L10n message
+update lang_messages set message = 'Last 3 months' where message = 'last 3 month';
+
+
+-- New category in order to modify the color for weekend
+SELECT im_category_new (5008, 'Weekend', 'Intranet Absence Type'); 
+update im_categories set aux_string2 = 'BBBBBB' where category_id = 5008;
+
