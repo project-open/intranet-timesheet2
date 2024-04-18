be8e7b0be9eb3774f01126ecad222687ecc93941
Frank Bergmann <frank.bergmann@project-open.com>
2024-04-18 12:22:29 +0200

    Fixed upgrade script

--- sql/postgresql/upgrade/upgrade-5.1.0.0.0-5.1.0.0.1.sql
+++ sql/postgresql/upgrade/upgrade-5.1.0.0.0-5.1.0.0.1.sql
@@ -7,5 +7,5 @@ SELECT acs_log__debug('/packages/intranet-timesheet2/sql/postgresql/upgrade/upgr
 select acs_privilege__create_privilege('add_hours_direct_reports','Add hours for direct reports','Add hours for direct reports');
 select acs_privilege__add_child('admin', 'add_hours_direct_reports');
 select acs_privilege__create_privilege('view_hours_direct_reports','View hours for direct reports','View hours for direct reports');
-select acs_privilege__view__child('admin', 'view_hours_direct_reports');
+select acs_privilege__add_child('admin', 'view_hours_direct_reports');
 
